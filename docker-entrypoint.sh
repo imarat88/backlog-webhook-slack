b9280d56ff1fd609e110bf290fa6945c6992e70e
Shoma Nishitateno <shoma416@gmail.com>
2018-01-08 11:10:26 +0900

    Pass variables as parameters

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,24 @@
 
 echo "** Preparing Backlog-slack webhook Server"
 
+if [ -z "${BACKLOG_URL}" ]; then
+    echo "**** Error: BACKLOG_URL is undefied."
+    exit 1
+fi
+
+echo "Backlog URL: ${BACKLOG_URL}"
+
+if [ -z "${SLACK_URL}" ]; then
+    echo "**** Error: SLACK_URL is undefied."
+    exit 1
+fi
+
+echo "slack URL: ${SLACK_URL}"
+
 echo "########################################################"
 
 echo "** Executing Backlog-slack webhook Server"
 
-exec java -jar /app/backlog-webhook-slack-assembly.jar "$@"
+exec java -Dbacklog.url="$BACKLOG_URL" \
+          -Dslack.url="$SLACK_URL" \
+          -jar /app/backlog-webhook-slack-assembly.jar "$@"
